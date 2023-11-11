d1fc2862384b7f09a9790106652f81f5b669aef8
nenadmne <cosovicnenad14@gmail.com>
2023-11-11 14:12:22 +0100

    Adding severity prop to warning mode.

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,18 +1,26 @@
 import { type ReactNode } from "react";
 
-type InfoBoxProp = {
-  mode: "hint" | "warning";
+type HintBoxProps = {
+  mode: "hint";
   children: ReactNode;
 };
 
-export default function InfoBox({ mode, children }: InfoBoxProp) {
-  if (mode === "hint") {
-    return <aside className="infobox infobox-hint">{children}</aside>;
+type WarningBoxProp = {
+  mode: "warning";
+  severity: "low" | "medium" | "high";
+  children: ReactNode;
+};
+
+type InfoBoxProp = HintBoxProps | WarningBoxProp;
+
+export default function InfoBox(props: InfoBoxProp) {
+  if (props.mode === "hint") {
+    return <aside className="infobox infobox-hint">{props.children}</aside>;
   }
   return (
-    <aside className="infobox infobox-warning warning--medium">
-      <h2>{mode}</h2>
-      {children}
+    <aside className={`infobox infobox-warning warning--${props.severity}`}>
+      <h2>{props.mode}</h2>
+      {props.children}
     </aside>
   );
 }
